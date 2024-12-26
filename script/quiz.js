8fdfd64789f729919ac691ddea93042bd5d933a1
SarojKanvadiya <sarojkanvadiya@gmail.com>
2024-12-26 17:00:00 +0530

    card

--- script/quiz.js
+++ script/quiz.js
@@ -33,4 +33,44 @@ form.addEventListener("submit",function(){
     })
 })
 
+async function getQuiz() {
+    try{
+        let res = await fetch(`${baseurl}/questions`);
+    let data = await res.json()
+    console.log(data)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+// getQuiz()
+
+window.onload = async()=>{
+    let arr = await getQuiz();
+    displayQuiz(arr)
+}
+let cont = document.getElementById("quiz-container");
+function displayQuiz(arr){
+arr.map((el,i)=>{
+    let card = document.createElement("div");
+    card.setAttribute("id", "card");
+    
+    let title = document.createElement("h3");
+    title.textContent = `Title: ${el.title}`;
+
+    let optionA = document.createElement("p");
+    optionA.textContent = `A : ${el.optionA}`
+    let optionB = document.createElement("p");
+    optionB.textContent = `B : ${el.optionB}`
+    let optionC = document.createElement("p");
+    optionC.textContent = `C : ${el.optionC}`
+    let optionD = document.createElement("p");
+    optionD.textContent = `D : ${el.optionD}`
+    
+    card.append(title,optionA,optionB,optionC,optionD)
+
+cont.appendChild(card)
+})
+}
+
 
